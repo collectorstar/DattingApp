91c9cc76297d38b44b1bddfc65e0d05065848065
collectorstar <duy57665@nuce.edu.vn>
2023-02-22 15:47:46 +0700

    end session 5

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -19,7 +19,7 @@ export class NavComponent implements OnInit{
 
   login(){
     this.accountService.login(this.model).subscribe(response =>{
-      console.log(response);
+      console.log(response); 
     },error =>{
       console.log(error);
     })
